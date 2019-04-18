95456a625ff5cd91701437c4b63d64301bd959b9
Ruslan Mutygullin <sounditbox@gmail.com>
2019-04-18 14:23:46 +0500

    switch done

---
+++ thrird_task/bin/stm32f0discovery/auto_init/auto_init_sht1x.d
@@ -0,0 +1,5 @@
+/home/ek-181/riot-examples-master/02button/bin/stm32f0discovery/auto_init/auto_init_sht1x.o: \
+ /home/ek-181/RIOT/sys/auto_init/auto_init_sht1x.c \
+ /home/ek-181/riot-examples-master/02button/bin/stm32f0discovery/riotbuild/riotbuild.h
+
+/home/ek-181/riot-examples-master/02button/bin/stm32f0discovery/riotbuild/riotbuild.h:
